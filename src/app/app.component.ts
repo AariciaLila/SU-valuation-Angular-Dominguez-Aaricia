517288ebe91796fc3bec4c24c3114284cd5c0f73
AariciaLila <82643809+AariciaLila@users.noreply.github.com>
2022-05-24 14:00:22 +0200

    Exercice 4 : Interpolation de variables
    Instanciez 2 statistiques “en dur” (c’est à dire, créez 2 statistiques directement côté front avec les données de votre
    choix) dans le composant APP de votre application et affichez le titre de vos deux statistiques sur votre page.
    Commitez votre travail.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Statistique } from './model/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'projetstatistiques';
+export class AppComponent implements OnInit {
+  statistique: Statistique[]= [
+    {
+      identifiant: 'fa1f5f40-be3b-11eb-91ec-7f5875ecfb46',
+      titre : 'Démographie en France',
+      valeur: '60M'
+    },
+{
+      identifiant: 'gz2g6g51-nr4n-22rn-02rv-8g6986rvgn57',
+      titre : 'Démographie en Suisse',
+      valeur: '8M'
+    }]
+
+  ngOnInit(){
+  }
 }
+
+
+
+
